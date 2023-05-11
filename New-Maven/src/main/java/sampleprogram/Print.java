22bd73729564ccb02ec12fb214675f89e2ed19a3
AmruthaWilson <amruthawilson04@gmail.com>
2023-05-11 09:45:17 +0530

    FIRST COMMIT

---
+++ New-Maven/src/main/java/sampleprogram/Print.java
@@ -0,0 +1,16 @@
+package sampleprogram;
+
+public class Print 
+{
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) 
+	{
+		Print obj=new Print ();
+		obj.display();
+	}
+
+}
